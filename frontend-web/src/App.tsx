429e2347321ecc89a6880d5f4b9362fe17bbd8c7
Myons <rstephenaizun12@gmail.com>
2025-04-07 23:17:33 +0800

    Front-end update

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/Login'
+import RegisterPage from './pages/Register'
 
 function App() {
   return (
-    <div style={{ textAlign: "center", padding: "20px", fontSize: "24px" }}>
-      <h1>Welcome to Bounty App</h1>
-    </div>
-  );
+    <Router>
+      <Routes>
+        {/* Default route for the login page */}
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
